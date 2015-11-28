692e912d2b8d7589d28cc2fd5b9e595fbfd2074d
Curtis McEnroe <programble@gmail.com>
2015-11-28 03:36:03 -0500

    Implement authorization_uri

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,36 @@
+extern crate url;
+
+use url::{Url, ParseResult};
+
+/// OAuth2 client.
 pub struct Client {
-    client_id: String,
-    client_secret: String,
+    pub authorization_uri: String,
+
+    pub client_id: String,
+    pub client_secret: String,
+    pub redirect_uri: Option<String>,
+}
+
+impl Client {
+    pub fn authorization_uri(&self, scope: Option<&str>, state: Option<&str>) -> ParseResult<String> {
+        let mut uri = try!(Url::parse(&self.authorization_uri));
+
+        let mut query_pairs = vec![
+            ("response_type", "code"),
+            ("client_id", &self.client_id),
+        ];
+        if let Some(ref redirect_uri) = self.redirect_uri {
+            query_pairs.push(("redirect_uri", redirect_uri));
+        }
+        if let Some(scope) = scope {
+            query_pairs.push(("scope", scope));
+        }
+        if let Some(state) = state {
+            query_pairs.push(("state", state));
+        }
+
+        uri.set_query_from_pairs(query_pairs.iter());
+
+        Ok(uri.serialize())
+    }
 }
