7ca874de6bccf0e60f69d44f035d3bf35e40eec4
Curtis McEnroe <programble@gmail.com>
2015-12-01 19:10:49 -0500

    Tweak example GitHub redirect URI

--- examples/github.rs
+++ examples/github.rs
@@ -8,7 +8,7 @@ fn main() {
         Default::default(),
         "01774654cd9a6051e478",
         "9f14d16d95d605e715ec1a9aecec220d2565fd5c",
-        Some("https://cmcenroe.me/oauth2-paste")
+        Some("https://cmcenroe.me/oauth2-paste/")
     );
 
     let auth_uri = client.auth_uri(Some("user"), None).unwrap();
