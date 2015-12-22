f3cdd6142df3ace232b550adf29ffb5f51826d9c
Curtis McEnroe <programble@gmail.com>
2015-12-21 23:34:38 -0500

    Add GitHub example

---
+++ examples/github.rs
@@ -0,0 +1,16 @@
+extern crate inth_oauth2;
+
+use inth_oauth2::client::Client;
+use inth_oauth2::provider::GitHub;
+
+fn main() {
+    let client = Client::<GitHub>::new(
+        Default::default(),
+        "01774654cd9a6051e478",
+        "9f14d16d95d605e715ec1a9aecec220d2565fd5c",
+        Some("https://cmcenroe.me/oauth2-paste/")
+    );
+
+    let auth_uri = client.auth_uri(Some("user"), None).unwrap();
+    println!("{}", auth_uri);
+}
