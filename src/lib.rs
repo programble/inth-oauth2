bfc17a30203abdd3c868be6ea7813e8f5cdaf61f
Curtis McEnroe <programble@gmail.com>
2015-12-21 21:40:38 -0500

    Add token traits

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ extern crate chrono;
 extern crate hyper;
 extern crate rustc_serialize;
 extern crate url;
+
+pub mod token;
---
+++ src/token/mod.rs
@@ -0,0 +1,23 @@
+//! Tokens.
+
+/// OAuth 2.0 tokens.
+///
+/// See [RFC6749, section 5](http://tools.ietf.org/html/rfc6749#section-5).
+pub trait Token<L: Lifetime> {
+    /// Returns the access token.
+    ///
+    /// See [RFC6749, section 1.4](http://tools.ietf.org/html/rfc6749#section-1.4).
+    fn access_token(&self) -> &str;
+
+    /// Returns the scope, if available.
+    fn scope(&self) -> Option<&str>;
+
+    /// Returns the token lifetime.
+    fn lifetime(&self) -> &L;
+}
+
+/// OAuth 2.0 token lifetimes.
+pub trait Lifetime {
+    /// Returns true if the token is no longer valid.
+    fn expired(&self) -> bool;
+}
