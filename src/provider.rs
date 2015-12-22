cd66c77a941a7d5d3fc401eb87b79220c4515659
Curtis McEnroe <programble@gmail.com>
2015-12-21 22:28:48 -0500

    Add GitHub provider

--- src/provider.rs
+++ src/provider.rs
@@ -1,6 +1,6 @@
 //! Providers.
 
-use token::{Token, Lifetime, Bearer, Expiring};
+use token::{Token, Lifetime, Bearer, Static, Expiring};
 
 /// OAuth 2.0 providers.
 pub trait Provider {
@@ -31,4 +31,15 @@ impl Provider for Google {
     type Token = Bearer<Expiring>;
     fn auth_uri() -> &'static str { "https://accounts.google.com/o/oauth2/v2/auth" }
     fn token_uri() -> &'static str { "https://www.googleapis.com/oauth2/v4/token" }
+}
+
+/// GitHub OAuth 2.0 provider.
+///
+/// See [OAuth, GitHub Developer Guide](https://developer.github.com/v3/oauth/).
+pub struct GitHub;
+impl Provider for GitHub {
+    type Lifetime = Static;
+    type Token = Bearer<Static>;
+    fn auth_uri() -> &'static str { "https://github.com/login/oauth/authorize" }
+    fn token_uri() -> &'static str { "https://github.com/login/oauth/access_token" }
 }
