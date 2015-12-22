7dcc49da8dff06ce54b876ad1d7221deca6c0f2f
Curtis McEnroe <programble@gmail.com>
2015-12-22 00:30:09 -0500

    Enable extra lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences
+)]
+
 extern crate chrono;
 extern crate hyper;
 extern crate rustc_serialize;
