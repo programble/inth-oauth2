f3906e71d0ea93144b14f4a91f465fdd03fbdf1a
Curtis McEnroe <programble@gmail.com>
2017-06-04 20:05:30 -0400

    Use TLS in examples

--- examples/google-installed.rs
+++ examples/google-installed.rs
@@ -1,11 +1,19 @@
+extern crate hyper;
+extern crate hyper_native_tls;
 extern crate inth_oauth2;
 
 use std::io;
 
+use hyper_native_tls::NativeTlsClient;
+use hyper::net::HttpsConnector;
 use inth_oauth2::Client;
 use inth_oauth2::provider::google::{Installed, REDIRECT_URI_OOB};
 
 fn main() {
+    let tls = NativeTlsClient::new().unwrap();
+    let connector = HttpsConnector::new(tls);
+    let https = hyper::Client::with_connector(connector);
+
     let client = Client::<Installed>::new(
         String::from("143225766783-ip2d9qv6sdr37276t77luk6f7bhd6bj5.apps.googleusercontent.com"),
         String::from("3kZ5WomzHFlN2f_XbhkyPd3o"),
@@ -19,11 +27,9 @@ fn main() {
     let mut code = String::new();
     io::stdin().read_line(&mut code).unwrap();
 
-    let http_client = Default::default();
-
-    let token = client.request_token(&http_client, code.trim()).unwrap();
+    let token = client.request_token(&https, code.trim()).unwrap();
     println!("{:?}", token);
 
-    let token = client.refresh_token(&http_client, token, None).unwrap();
+    let token = client.refresh_token(&https, token, None).unwrap();
     println!("{:?}", token);
 }
